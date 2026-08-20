f4bf37ee6473abebfb3315f8bc3d88e604d10f5a
dona-eric <donaerickoulodji@gmail.com>
2026-08-20 15:24:15 +0100

    chore: disable ReCaptchaWrapper component in layout

--- AcademyFrontend/src/app/layout.tsx
+++ AcademyFrontend/src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
     "La plateforme de référence francophone pour se former en Machine Learning, Data Science et IA. Cours, notebooks interactifs et certifications.",
 };
 
-import ReCaptchaWrapper from "@/components/ReCaptchaWrapper";
+// import ReCaptchaWrapper from "@/components/ReCaptchaWrapper";
 
 export default function RootLayout({
   children,
@@ -20,14 +20,14 @@ export default function RootLayout({
   return (
     <html lang="fr" className="antialiased font-sans">
       <body className="bg-[var(--bg-base)] text-[var(--text-primary)] min-h-screen flex flex-col">
-        <ReCaptchaWrapper>
+        {/* <ReCaptchaWrapper> */}
           <AuthProvider>
             <ConditionalHeader />
             <MainWrapper>{children}</MainWrapper>
             <GlobalAIAssistant />
             <ConditionalFooter />
           </AuthProvider>
-        </ReCaptchaWrapper>
+        {/* </ReCaptchaWrapper> */}
       </body>
     </html>
   );
