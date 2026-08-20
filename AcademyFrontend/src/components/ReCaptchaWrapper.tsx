a202c96264ec0578ecd9625f3c8d0cdc2d806441
dona-eric <donaerickoulodji@gmail.com>
2026-08-20 13:49:40 +0100

    chore: disable reCAPTCHA server-side and client-side validation logic

--- AcademyFrontend/src/components/ReCaptchaWrapper.tsx
+++ AcademyFrontend/src/components/ReCaptchaWrapper.tsx
@@ -3,7 +3,7 @@
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 export default function ReCaptchaWrapper({ children }: { children: React.ReactNode }) {
-  const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || 'dummy_site_key';
+  const siteKey = <process className="env NEXT_PUBLIC_RECAPTCHA_SITE_KEY"></process>;
 
   return (
     <GoogleReCaptchaProvider reCaptchaKey={siteKey}>
